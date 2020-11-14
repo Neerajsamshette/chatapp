56fdcae4342548eaa0c1c5e82753ff593d805214
Neerajsamshette <neeraj.samshette@gmail.com>
2020-11-14 12:58:28 +0530

    second time

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const users = {};
 
 io.on('connection', socket => {
     // console.log('Connection with ' + socket.id)
-    socket.on('new-user-joined' , name => {
+    socket.on('new-user-joined', name => {
         users[socket.id] = name;
-        socket.broadcast.emit('user-joined' , name)
+        socket.broadcast.emit('user-joined', name)
         // console.log(users[socket.id])
     });
 
-    socket.on('send' , message=>{
-        socket.broadcast.emit('recieve' , {message:message , user: users[socket.id]})
+    socket.on('send', message => {
+        socket.broadcast.emit('recieve', { message: message, user: users[socket.id] })
     });
 
-    socket.on('disconnect' , message =>{
-        socket.broadcast.emit('left' , users[socket.id]);
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     });
 
